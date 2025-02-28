256ae213efd5764d33874280a1c07f90b78f427d
natnaelengeda <nattynengeda@gmail.com>
2025-02-28 23:58:36 +0300

    Finish Page Layout and Add Bible Json Data

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+
+// Components
 import Header from './Header'
+import AppSidebar from './Sidebar'
+
+// Shadcn
+import { SidebarProvider } from "@/components/ui/sidebar"
 
 interface IPageProps {
   children: React.ReactNode
@@ -7,10 +13,13 @@ interface IPageProps {
 
 export default function DefaultLayout({ children }: IPageProps) {
   return (
-    <div
-      className='w-full h-full min-h-screen'>
-      <Header />
-      {children}
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <div
+        className='w-full h-full min-h-screen'>
+        <Header />
+        {children}
+      </div>
+    </SidebarProvider>
   )
 }
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Image from 'next/image'
 
+// Shadcn
+import { SidebarTrigger } from "@/components/ui/sidebar"
 // AppAsset
 import AppAsset from '@/core/AppAsset'
 
@@ -9,8 +11,10 @@ export default function Header() {
   return (
     <header
       className="w-full flex h-16 shrink-0 items-center gap-2 border-b border-gray-400 px-4">
+
       <div
-        className="w-full mx-auto container px-20 flex items-center gap-5">
+        className="w-full mx-auto flex items-center gap-5">
+        <SidebarTrigger size={"icon"} />
         <Image
           src={AppAsset.logo}
           alt="logo"
